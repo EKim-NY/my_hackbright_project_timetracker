e67bc44351ae825a3483e58221e94cf3a3aa3196
EKim_NY <erenkim@gmail.com>
2020-06-15 21:21:04 -0400

    revised Jinja files

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Server for TimeTracker app."""
 
-from flask import Flask, render_template, request, flash, session, redirect
-from model import db 
+from flask import (Flask, render_template, request, flash, session, redirect)
+from model import db, connect_to_db 
 import crud
 
 # Make jinja2 throw errors for undefined vars
@@ -20,8 +20,11 @@ app.jinja_env.undefined = StrictUndefined
 def homepage(): 
     """View homepage; user will be redirected to either the login or new account page."""
 
+     
     return render_template('homepage.html')
 
+    # Debugging
+    # return '<html><body><h1>HOMEPAGE</h1></body></html>'
 
 @app.route('/login')
 def login():
@@ -39,27 +42,45 @@ def new_account():
 
 
 
-@app.route('/select_session')
-def select_session(): 
-    """Allow user to select a new or existing Pomodoro session."""
+# @app.route('/projects')
+# def projects(): 
+#     """Allow user to specify project details."""
+
+#     return render_template('projects.html')
 
 
+# @app.route('/session_details')
+# def session_details(): 
+#     """Allow user to select a new or existing Pomodoro session."""
 
-
-# @app.route('/create_session')
-
-
-# @app.route('/repeat_session')
-
-
-# @app.route('/resume_session')
+#     return render_template('session_details.html')
 
 
 # @app.route('/pause_session')
+# def pause_session():
+#     """Allow user to pause/resume a currently active session."""
+
+#     return render_template('pause_session.html')
 
 
-# @app.route('/session_complete')
+# @app.route('/delete_session')
+# def delete_session():
+#     """Delete session from database if user cancels session."""
+
+#     return render_template('delete_session.html')
+
 
 
 # @app.route('/logout')
+# def logout():
+#     """Display end-of-session message upon logout."""
 
+#     return render_template('logout.html')
+
+
+
+
+ # Connect to database before calling app.run or Flask can't access db!
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
