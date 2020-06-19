8a2c0e40b15c2fcef0e93907e9b79a0527d5a295
EKim_NY <erenkim@gmail.com>
2020-06-19 19:44:43 -0400

    separate f(x)'s added for showing date in browser and for getting date info for data manipulation

--- static/js/timer.js
+++ static/js/timer.js
@@ -7,7 +7,7 @@ let shortBreakSession = 5 * 60; // 5 min in seconds
 let longBreakSession = 15 * 60; // 15 min in seconds 
 
 let timeInSession; // assign it a value later
-let sessionDuration; // chronological length of session 
+let sessionDuration; // session length [s]
 
 // add timezone 
 
@@ -18,6 +18,12 @@ let month; // int; 0 is January
 let hour; // int; 12-hr clock 
 let timeOfDay; // str
 let militaryHour // int; 24-hr clock 
+
+let dateVars; 
+let timeVars; 
+let dateToday; 
+let time; 
+let dateTimeList; 
 
 let timer = document.getElementById("timerTime");
 let clock = document.getElementById("clockTime"); 
@@ -109,50 +115,6 @@ function getSelectedSession (session) {
     } 
 }; 
 
-function showTimer(sessionDuration) {  
-    console.log('showTimer started'); 
-    // setInterval(f(x), time-interval) executes f(x) every time-interval 
-    // EX: setInterval(myTimer, 1000) executes myTimer every 1000 milliseconds
-
-    // this snippet works in Repl.it but not sure about timer.innerText 
-    timeLeftInSession = sessionDuration; 
-
-    if (timeLeftInSession == 0) { 
-        // If END OF SESSION
-        // Show end of timer countdown. 
-        // Save session details in SQL. 
-        stopSession = true; 
-        clearInterval(show); 
-        console.log('Session has ended'); 
-
-    } else if (startSession == true && pauseSession == true && stopSession == false) {
-        // If SESSION PAUSED
-        // Show paused timer in browser. 
-
-        timeForResumingSession = timeLeftInSession; 
-        clearInterval(show);
-        console.log('Session has paused'); 
-
-    } else if (startSession == true && pauseSession == false && stopSession == false) {
-        // If SESSION STARTED
-        // Show start of timer countdown. 
-
-
-    } else if (startSession == true && pauseSession == true && stopSession == false) {
-        // If RESUME SESSION
-        // Continue timer countdown. 
-
-
-    } else if (startSession == true && pauseSession == false && stopSession == true) {
-        // TOP SESSION FOR GOOD
-        // Don't send session details to SQL. 
-        // Future work: Ask user if user wants to save/cancel session. 
-        // End timer countdown. 
-        // Set timer to 0:00:00. 
-        stopTime = new Date(); 
-    }; // end of if 
-
-}; // end of f(x) 
 
 
 
@@ -207,40 +169,37 @@ function convertTimeToMillis(timeAsList) {
 } // end of f(x) 
 
 
-// setInterval(f(x), 1000) executes function() every 1000 ms or 1 s 
-// setInterval will continue executing unless clearInterval stops it
-//------------------------------------------------------------
 
 
-let timeVars; 
-let dateToday; 
-let time; 
-let dateTimeList; 
-// Test: It works! (independently of prior f(x)'s)
-// > DateAndTime(); 
-// 10:10 PM   => browser displays actual current time but only when f(x) is called
-function getDisplayDate() {
-
-        today = new Date(); // UNIX time since epoch [ms]         
-        startHour = today.getHours(); // hours in military time 
-        startMinute = today.getMinutes(); 
-        dayOfMonth = today.getDate(); // day of month from 0-31 
-        //weekday = today.getDay(); // 0 is Sunday  
-        month = today.getMonth(); // 0 is January
-        year = today.getUTCFullYear(); 
-        timeVars = [month, dayOfMonth, year, startHour, startMinute]; 
-        dateToday = today.toLocaleDateString(); // '6/18/2020'
-        todayDate.innerText = dateToday; 
-    }; // end of f(x)
 
 
+function showDate() {
+    // Display current date in browser. 
+
+    today = new Date(); // UNIX time since epoch [ms]         
+    dateToday = today.toLocaleDateString(); // Ex. '6/18/2020'
+    todayDate.innerText = dateToday; 
+};
+
+
+function getDateInfo() {
+    // Get date info as separate vars for data manipulation.
+
+    dayOfMonth = today.getDate(); // day of month from 0-31 
+    weekday = today.getDay(); // 0 is Sunday  
+    month = today.getMonth(); // 0 is January
+    year = today.getUTCFullYear(); 
+    dateVars = [month, dayOfMonth, year, weekday]; 
+
+    return dateVars; 
+}
 
 
 // Test: It works! 
 // > timeVars = [6, 1, 2020, 20, 0]; 
 // > convertDate(timeVars); 
 // (3) ["June", 1, 2020]
-function convertMonth_returnDate(timeVars) {
+function convertMonth_returnDate(dateVars) {
     // return human-readable time; make month human-friendly
     // timeVars = [month, dayOfMonth, year, startHour, startMinute]
 
@@ -257,9 +216,9 @@ function convertMonth_returnDate(timeVars) {
                   'November', 
                   'December']; 
 
-    month = months[(timeVars[0] -1)]; // Tested! Returns correct month 
+    month = months[(dateVars[0] -1)]; // Tested! Returns correct month 
     // return [month, day, year]
-    return dateList = [month, timeVars[1], timeVars[2]]; 
+    return dateList = [month, dateVars[1], dateVars[2], dateVars[3]]; 
  }; // end of f(x) 
 
 
@@ -271,8 +230,9 @@ function convertMonth_returnDate(timeVars) {
 // (3) [6, 30, "PM"]
 function convertMilitaryTime (timeVars) {
     // convert hours from 24-hr to 12-hr clock 
-    // timeVars = [month, dayOfMonth, year, startHour, startMinute]
-    let militaryHour = timeVars[3]; 
+    // timeVars = [hours, minutes, seconds]
+    let militaryHour = timeVars[0]; 
+
     if (militaryHour == 0 || militaryHour == 24) {
         hour = 12; 
         timeOfDay = 'AM'; 
@@ -286,7 +246,7 @@ function convertMilitaryTime (timeVars) {
         hour = militaryHour - 12; 
         timeOfDay = 'PM'; 
     }
-    // return [hour, minutes, AM/PM]
-    return timeList = [hour, timeVars[4], timeOfDay]; 
+    // return [hour, minutes, seconds, AM/PM]
+    return timeList = [hour, timeVars[1], timeVars[2], timeOfDay]; 
 }; // end of convertMilitaryTime() 
 
