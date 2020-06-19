4080c085d6ae3e59310ec39c56a00ea95e26f7b3
EKim_NY <erenkim@gmail.com>
2020-06-19 00:27:00 -0400

    Methods for estimating time edited in timer.js

--- static/js/timer.js
+++ static/js/timer.js
@@ -1,3 +1,5 @@
+"use strict"; 
+
 let workSession = 25 * 60; // 25 min in seconds
 let shortBreakSession = 5 * 60; // 5 min in seconds 
 let longBreakSession = 15 * 60; // 15 min in seconds 
@@ -5,15 +7,8 @@ let longBreakSession = 15 * 60; // 15 min in seconds
 let timeInSession; // assign it a value later
 let sessionDuration; // chronological length of session 
 
-
-
-let getTime; //  int; total millisec since epoch time aka 1/1/1970
-// use timestamp to calculate how much time passed 
-// -> convert to human-readable form later before displaying it to user
 // add timezone 
-let epochTime; // int; total milliseconds since epoch (ie, 1/1/1970) 
-// Note: difference of epochTimes taken on the same day at different times is zero 
-//       BUT converting the epochTime into a date/time show different times
+
 let startHour; // int; get hour in military time 
 let startMinute; // int
 let day; // int; 0 is Sunday  
@@ -24,51 +19,60 @@ let militaryHour // int; 24-hr clock
 
 let timer = document.getElementById("timerTime");
 let clock = document.getElementById("clockTime"); 
-let play = document.getElementById("pomodoro-play"); // select Play button 
+let todayDate = document.getElementById("todayDate");
 
+let startSession = true; 
+let pauseSession = false; 
+let stopSession = false; 
+
+// Select DOM elements. 
+let play = document.getElementById("pomodoro-play"); 
+let pause = document.getElementById("pomodoro-pause"); 
+let stop = document.getElementById("pomodoro-stop"); 
+
+let session; 
+let dropdown; 
+
+// Event Listeners 
 play.addEventListener('click', () => {
-
-    // PSEUDOCODE
-    // If session is 'Work': 
-    //  Set timer to prefixed value 
-    //  (eg, 25 min, 15 min, 5 min. or custom)
-    //  Start timer 
-    //    Log date and time of start session in SQL 
-    //    Set startBool = false 
-    //    Display timer in DOM 
-    //    Decrement actual timer 
-    //    When it hits zero, stop function
-    //      Log date and time of end session in SQL
-
-    playBool = true; // user clicked Play button 
-    // For selected session, set timer to prefixed value and start timer
-    // Save session details in SQL => do this in Flask? 
-    
-    // DOM dropdown value 
-    // const sessionTypeOption = document.querySelector('.session_type'); 
-
-    let dropdown = document.getElementById("session_type"); 
-    // Select DOM node containing all dropdown options 
-    let session = dropdown.options[dropdown.selectedIndex].value; 
-    // From the dropdown options, return the value (ie, displayed text) 
-    // of the selected option (identify by index of dropdown option)
-    // Note: Dropdown option indices start with 0 (just like for an array)
-
-    if (session == 'Work') {
-        callTimer(workSession); 
-    } else if (session == "Short Break"){
-        callTimer(shortBreakSession);
-    } else if (session == "Long Break"){
-        callTimer(longBreakSession); 
+    console.log('Click Play'); 
+    startSession = true; 
+    // Check if new session 
+    if (pauseSession == false && stopSession == false) { 
+        console.log('if-then: pause=false, stop=false'); 
+        // Select DOM node containing all dropdown options 
+        // Dropdown option indices start with 0 (just like for an array)
+        dropdown = document.getElementById("session_type");
+        session = dropdown.options[dropdown.selectedIndex].value; 
+        getSelectedSession(session); 
+    } else if (pauseSession == true && stopSession == false) {
+        console.log('else-if')
+        // resume timer 
     }
-
-
 }); 
 
 
+pause.addEventListener('click', (session) => {
+    
+    startSession = true; 
+    pauseSession = true; 
+    stopSession = false; 
+
+    // stop timer
+    // don't record session 
+
+}); 
+
+stop.addEventListener('click', (session) => {
+    startSession = true; 
+    pauseSession = false; 
+    stopSession = false; 
+
+    // stop timer
+    // send session details to SQL 
+}); 
 
 
-// Check if these values can be passed into SQL. 
 let startTime; 
 let stopTime; 
 let today; 
@@ -77,36 +81,74 @@ let totalSeconds;
 let totalMinutes; 
 let totalHours; 
 
-// Test: no discernible syntax errors but callTimer() doesn't work properly. 
-function callTimer(sessionDuration) {
-    startBool = true; // Timer on  
+function getSelectedSession (session) {
+    console.log('getSelectedSession');
+    console.log(session);
+    if (session == 'work') {
+        console.log('if: Work');
+        callTimer(workSession); 
+    } else if (session == "short_break"){
+        console.log('if:Short break');
+        callTimer(shortBreakSession);
+    } else if (session == "long_break"){
+         console.log('if:Long Break');
+        callTimer(longBreakSession); 
+    } 
+}; 
 
+
+
+
+
+// Check if these values can be passed into SQL. 
+
+let timeForResumingSession;
+
+// Test: no discernible syntax errors but callTimer() doesn't work properly. 
+function callTimer(sessionDuration) {  
+    console.log('callTimer started'); 
     // setInterval(f(x), time-interval) executes f(x) every time-interval 
     // EX: setInterval(myTimer, 1000) executes myTimer every 1000 milliseconds
 
     // this snippet works in Repl.it but not sure about timer.innerText 
     let timeInSession = sessionDuration; 
-    startTime = Date.now(); 
+    // Replaced startTime = Date.now() with startTime = new Date()
+    startTime = new Date(); 
+    console.log('start timer'); 
     let show = setInterval(() => {
         // For every second that passes, show clock and timer countdown. 
         // Display clock time. 
-        let today = Date.now(); 
+        let today = new Date(); 
         clock.innerText = today.toLocaleTimeString(); 
         timeInSession--; 
         if (timeInSession == 0) { 
-            stopTime = Date.now(); 
-            startBool = false; 
+            stopTime = new Date(); 
+            stopSession = true; 
             clearInterval(show); 
-        } else { 
+        } else if (pauseSession == true && startSession == true) {
+            timeForResumingSession = timeInSession; 
+            clearInterval(show);
+            console.log('Paused'); 
+        } else if (pauseSession == false && startSession == true) {
         // Continue displaying timer if countdown hasn't ended. 
+
             tiff = stopTime - startTime; 
-            totalSeconds = Math.floor(tiff/1000); 
-            totalMinutes = Math.floor(tiff/60);
-            totalHours = Math.floor(tiff/60); 
-            timer.innerText = 'Timer: ${totalHours}hrs ${totalMinutes}min ${totalSeconds}sec'; 
+            let sec = tiff/1000; 
+            let min = tiff/60; 
+            let hr = tiff/60;
+            // Snippet below leads to errors for totalHours:  
+            // > totalMinutes=Math.round(totalSeconds/60); 
+            // 93  
+            // > totalHours=Math.round(totalMinutes/60); 
+            // 1547
+            totalSeconds = Math.round(sec); 
+            totalMinutes = Math.round(min);
+            totalHours= Number.parseFloat(hr).toFixed(2);
+            timer.innerText = `Timer: ${totalHours} hrs ${totalMinutes} min ${totalSeconds} sec`; 
         }; 
 
-    }, 1000);
+    }, 1000); // end of setInterval()
+};
 
 
 
@@ -118,36 +160,26 @@ function callTimer(sessionDuration) {
 
 
 let timeVars; 
-let date; 
+let dateToday; 
 let time; 
 let dateTimeList; 
 // Test: It works! (independently of prior f(x)'s)
 // > DateAndTime(); 
 // 10:10 PM   => browser displays actual current time but only when f(x) is called
-function DateAndTime() {
-    // get current time 
-    // convert JS format to SQL format 
-    // export info to pSQL
+function getDisplayDate() {
 
-
-        let today = new Date(); // changes w/each call
-        // save the following in SQL: 
-        epochTime = today.getTime(); // [ms]
-        startHour = today.getHours(); // military time 
+        today = new Date(); // UNIX time since epoch [ms]         
+        startHour = today.getHours(); // hours in military time 
         startMinute = today.getMinutes(); 
         dayOfMonth = today.getDate(); // day of month from 0-31 
-        // weekday = today.getDay(); // 0 is Sunday  
+        //weekday = today.getDay(); // 0 is Sunday  
         month = today.getMonth(); // 0 is January
         year = today.getUTCFullYear(); 
         timeVars = [month, dayOfMonth, year, startHour, startMinute]; 
+        dateToday = today.toLocaleDateString(); // '6/18/2020'
+        todayDate.innerText = dateToday; 
+    }; // end of f(x)
 
-        date = today.toLocaleDateString(); // '2:23:26 AM'
-        time = today.toLocaleTimeString(); // '6/18/2020'
-        dateTimeList = [date, time]; 
-        
-        shortDisplayTimer(dateTimeList); 
-    }; // end of if 
-}; // end of DateAndTime() 
 
 
 
@@ -155,7 +187,7 @@ function DateAndTime() {
 // > timeVars = [6, 1, 2020, 20, 0]; 
 // > convertDate(timeVars); 
 // (3) ["June", 1, 2020]
-function convertDate(timeVars) {
+function convertMonth_returnDate(timeVars) {
     // return human-readable time; make month human-friendly
     // timeVars = [month, dayOfMonth, year, startHour, startMinute]
 
@@ -175,46 +207,7 @@ function convertDate(timeVars) {
     month = months[(timeVars[0] -1)]; // Tested! Returns correct month 
     // return [month, day, year]
     return dateList = [month, timeVars[1], timeVars[2]]; 
- }; // end of convertDate() 
-
-
-
-// Test: It works! (even for edge case of 00 minutes)
-// // > timeVars = [6, 1, 2020, 20, 0]; 
-// > displayTimer(timeVars); 
-// DOM: 8:00 PM
-function shortDisplayTimer(timeVars) {
-    // Date(epochTime); // convert epochTime to a human-friendly datetime str
-    //'Wed Jun 17 2020 00:00:51 GMT+0000 (Coordinated Universal Time)'
-
-    convertDate(timeVars); 
-    return dateList = [month, day, year]
-    let convertedTime = convertMilitaryTime(timeVars); 
-    return timeList = [hour, minutes, AM/PM]
-
-    // display time in browser 
-    // --- NOTES ---------------
-    // JS cares about whitespace
-    // If `string` is placed on a separate line, 
-    // then timer.textContent = Null 
-
-    if (convertedTime[1] >= 0 && convertedTime[1] < 10){
-        // EDGE CASE: Display minutes less than 10 as 00 to 09 in browser
-        let displayMinutes = '0' + convertedTime[1]; 
-        timer.innerText =`${convertedTime[0]}:${displayMinutes} ${convertedTime[2]}`;
-    } else {
-    timer.innerText =`${convertedTime[0]}:${convertedTime[1]} ${convertedTime[2]}`;
-    }
-};
-
-
-
-
-function longDisplayTimer(dateTimeList) {
-    // Alternate option: Discovered these methods after I'd already coded the above stuff! 
-
-    timer.innerText = dateTimeList[1]; // display local time 
-}; 
+ }; // end of f(x) 
 
 
 
